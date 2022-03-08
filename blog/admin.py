3bd0e87ec6cae7ee6d159776f91700fcc9c52362
DanJtl <daniel_junttila@live.se>
2022-03-08 13:34:18 +0000

    add PostAdmin class in admin.py

--- blog/admin.py
+++ blog/admin.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
-from .models import Post
 from django_summernote.admin import SummernoteModelAdmin
+from .models import Post
+
 
 @admin.register(Post)
 class PostAdmin(SummernoteModelAdmin):
 
+    search_fields = ['title', 'content']
+    list_display = ('title', 'slug', 'status', 'created_on')
+    prepopulated_fields = {'slug': ('title',)}
+    list_filter = ('status', 'created_on')
     summernote_fields = ('content')
