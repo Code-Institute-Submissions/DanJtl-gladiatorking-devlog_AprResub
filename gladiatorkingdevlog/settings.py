01a93c7781d1b59bfddc372b7cb46a41b929c894
DanJtl <daniel_junttila@live.se>
2022-03-14 12:23:00 +0000

    deploy commit 2

--- gladiatorkingdevlog/settings.py
+++ gladiatorkingdevlog/settings.py
@@ -30,7 +30,6 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-
 ALLOWED_HOSTS = ['gladiatorking-devlog.herokuapp.com', 'localhost']
 
 
