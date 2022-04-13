7f85e0c9bffa450af1f93b0f535c5329c2ec465f
DanJtl <daniel_junttila@live.se>
2022-04-13 12:02:56 +0000

    add delete_own_comment function

--- blog/urls.py
+++ blog/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path("", views.PostList.as_view(), name='home'),
     path('<slug:slug>/', views.PostDetail.as_view(), name='post_detail'),
     path('like/<slug:slug>', views.PostLike.as_view(), name='post_like'),
+    path("delete/<int:id>", views.delete_own_comment, name="delete_comment"),
 ]
